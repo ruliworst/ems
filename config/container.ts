fe4c967e75e121474d44ca4b110346c846d05b88
ruliworst <raulfa1503@gmail.com>
2024-05-22 18:42:27 +0200

    Feat: Registro de dependencias para los distintos repositorios. #8

--- config/container.ts
+++ config/container.ts
@@ -5,11 +5,20 @@ import PrismaDeviceRepository from "@/adapters/repositories/devices/PrismaDevice
 import DeviceService from "@/application/services/devices/DeviceService";
 import { GenerateAnomaliesReportTaskRepository } from "@/ports/tasks/GenerateAnomaliesReportTaskRepository";
 import PrismaGenerateAnomaliesReportTaskRepository from "@/adapters/repositories/tasks/PrismaGenerateAnomaliesReportTaskRepository";
+import PrismaGenerateConsumptionReportTaskRepository from "@/adapters/repositories/tasks/PrismaGenerateConsumptionReportTaskRepository";
+import PrismaMonitorizeConsumptionTaskRepository from "@/adapters/repositories/tasks/PrismaMonitorizeConsumptionTaskRepository";
+import PrismaMaintenanceDeviceTaskRepository from "@/adapters/repositories/tasks/PrismaMaintenanceDeviceTaskRepository";
 
 // Register repositories.
 container.registerSingleton<DeviceRepository>("DeviceRepository", PrismaDeviceRepository);
 container.registerSingleton<GenerateAnomaliesReportTaskRepository>(
   "GenerateAnomaliesReportTaskRepository", PrismaGenerateAnomaliesReportTaskRepository);
+container.registerSingleton<PrismaGenerateConsumptionReportTaskRepository>(
+  "GenerateConsumptionReportTaskRepository", PrismaGenerateConsumptionReportTaskRepository);
+container.registerSingleton<PrismaMonitorizeConsumptionTaskRepository>(
+  "MonitorizeConsumptionTaskRepository", PrismaMonitorizeConsumptionTaskRepository);
+container.registerSingleton<PrismaMaintenanceDeviceTaskRepository>(
+  "MaintenanceDeviceTaskRepository", PrismaMaintenanceDeviceTaskRepository);
 
 // Register services.
 container.registerSingleton(DeviceService);
