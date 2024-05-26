9d221d34a1ceb67ad7da89b03c136a04b0549dbf
ruliworst <raulfa1503@gmail.com>
2024-05-26 19:30:08 +0200

    Fix: Eliminar todo tipo de tareas al finalizar los tests. #8

--- jest.teardown.ts
+++ jest.teardown.ts
@@ -1,10 +1,13 @@
-// jest.teardown.js
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 module.exports = async () => {
+  console.log("TEAR DOWN")
   await prisma.device.deleteMany();
   await prisma.operator.deleteMany();
+  await prisma.generateAnomaliesReportTask.deleteMany();
+  await prisma.generateConsumptionReportTask.deleteMany();
+  await prisma.maintenanceDeviceTask.deleteMany();
   await prisma.monitorizeConsumptionTask.deleteMany();
 };
