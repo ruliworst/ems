9bec452ce8795024fc0c2049c2d1b18acd5530f6
ruliworst <raulfa1503@gmail.com>
2024-05-26 19:33:00 +0200

    Fix: Quitar console.log. #8

--- jest.teardown.ts
+++ jest.teardown.ts
@@ -3,7 +3,6 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 module.exports = async () => {
-  console.log("TEAR DOWN")
   await prisma.device.deleteMany();
   await prisma.operator.deleteMany();
   await prisma.generateAnomaliesReportTask.deleteMany();
