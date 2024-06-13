c6c45a4255b1f1bd91d481b3f82a2a5eec115e6e
ruliworst <raulfa1503@gmail.com>
2024-06-13 18:48:13 +0200

    Feat: Comparación de consumos. #17

--- src/infrastructure/api/services/analysis/AnalysisApiService.ts
+++ src/infrastructure/api/services/analysis/AnalysisApiService.ts
@@ -1,3 +1,4 @@
+import { CompareConsumptionBetweenDevicesDTO } from "../../dtos/analysis/analysis.dto";
 import { EnergyConsumptionRecordDTO } from "../../dtos/energy-consumption-records/energy-consumption-record.dto";
 
 export class AnalysisApiService {
@@ -5,6 +6,20 @@ export class AnalysisApiService {
     const response = await fetch(`/api/devices/${deviceName}/monitorize`);
     if (!response.ok) {
       throw new Error('Failed to fetch energy consumption record');
+    }
+    return response.json();
+  };
+
+  static async compareConsumptionBetweenDevices(dto: CompareConsumptionBetweenDevicesDTO) {
+    const response = await fetch(`/api/analysis/compare`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dto),
+    });
+    if (!response.ok) {
+      throw new Error('Failed to fetch comparison data');
     }
     return response.json();
   }
